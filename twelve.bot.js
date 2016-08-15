e9f350c55f496e1d569de5996e852545b103e016
Jeraldy Cascayan <casourcer@gmail.com>
2016-08-14 23:22:34 -1000

    cleaned
    now readable

--- twelve.bot.js
+++ twelve.bot.js
@@ -9,7 +9,7 @@ let Bot  = require('@kikinteractive/kik');
 let bot = new Bot({
     username: 'waznack',
     apiKey: '7cfc18d8-1011-4020-852f-aaa4be5f42d3',
-    baseUrl: 'https://14a72c56.ngrok.io/'
+    baseUrl: 'https://6644357b.ngrok.io/'
 });
  
 bot.updateBotConfiguration();
@@ -18,29 +18,22 @@ var d = new Date();
 var hour = d.getHours();
 var minute = d.getMinutes();
 var sec = d.getSeconds();
-var timestamp = hour + ":" + minute + ":" + sec
+var timestamp = hour + ":" + minute + ":" + sec //00:00:00
 
+var botStart = "v0.2 waznack".rainbow + " online @ " + timestamp.cyan; //v0.2 waznack online @ 00:00:00
+var fullusername = "Jeraldy Cascayan".green // uses @ line 26
+var HoldUser; //holds 'insult [username]' when insulting
+console.log('Created by ' + fullusername + ' a.k.a '.red + '@c0p'.yellow + ' on github'); //Created by Jeraldy Cascayan a.k.a @c0p on github
 
-var botStart = "v0.2 waznack".rainbow + " online @ " + timestamp.cyan;
-var fullusername = "Jeraldy Cascayan".green
-var HoldUser;
+var insult = require('./ArraysOfInsults.js').insult //import the variable insults onto here.
 
-
-console.log('Created by ' + fullusername + ' a.k.a '.red + '@c0p'.yellow + ' on github');
-
-var insult = require('./ArraysOfInsults.js').insult
-
-
-
-
-
+//Generates a random number. var rand = insult[0 - insult.length]
 function InsGen() {
 	var rand = insult[Math.floor(Math.random() * insult.length)]
 	return rand
 }
 
- //bot
-
+//If user isn't subscribed to bot, then send introduction 
 bot.onStartChattingMessage((message, next) => {
     bot.getUserProfile(message.from)
         .then((user) => {
@@ -49,71 +42,62 @@ bot.onStartChattingMessage((message, next) => {
 });
 
 
-//OnTextMessage
-var insult = require('./ArraysOfInsults.js').insult;
-
-
-
-
-
+//When User replies to bot
 bot.onTextMessage((message, next) => {
+var WhoInsult = message.body.split(" ") //WhoInsult turns message to ['i', 'am', 'an', 'example'];
 
-	var WhoInsult = message.body.split(" ")
+//if user says info
+if (message.body === 'info') {
 
+		message.reply('Commands: insult [username]'); //then bot says Commands: insult [username]
 
+} else if (WhoInsult[0] === "insult") { //else if user says 'insult'
+var botsentdate = new Date();
+var bothour = botsentdate.getHours();
+var botminute = botsentdate.getMinutes();
+var botsecond = botsentdate.getSeconds();
+var fullhold = bothour + ":" + botminute + ":" + botsecond
 
+/*CREATES TIME STAMP 00:00:00 ^ | SENDS INSULT MESSAGE V*/
 
-	//if user says info
-	if (message.body === 'info') {
-		message.reply('Commands: insult [username]');
-	} else if (WhoInsult[0] === "insult") {
-	var botsentdate = new Date();
-	var bothour = botsentdate.getHours();
-	var botminute = botsentdate.getMinutes();
-	var botsecond = botsentdate.getSeconds();
-	var fullhold = bothour + ":" + botminute + ":" + botsecond
-	var botInsultReply = InsGen()
-	var botReplyColor = botInsultReply.magenta,
-    UserReplyColor = message.body.green;
-	var UserHold = WhoInsult[1]; //username
-
-
-
-		bot.getUserProfile(message.from)
-	.then((user) => {
-
-			bot.send(botInsultReply, UserHold);
-    console.log(fullhold.cyan + ` ${user.username} ` + ' sent an insult message to : ' + UserHold.yellow);
-    console.log(fullhold.cyan+ " waznack replied : " + botReplyColor.magenta);
-        });
-  
-} else { 
-	// else do insults
-	var botsentdate = new Date();
-	var bothour = botsentdate.getHours();
-	var botminute = botsentdate.getMinutes();
-	var botsecond = botsentdate.getSeconds();
-	var fullhold = bothour + ":" + botminute + ":" + botsecond
-    UserReplyColor = message.body.green;
-    var BotReplyInsult = InsGen();
+var botInsultReply = InsGen() //now holds random insult 
+var botReplyColor = botInsultReply.magenta, //console.log magneta colored bot reply
+UserReplyColor = message.body.green; //console.log green colored user reply
+var UserHold = WhoInsult[1]; //UserHold now holds [username]
 
 bot.getUserProfile(message.from)
 	.then((user) => {
-			var ReplybotInsultReply = BotReplyInsult.magenta
-	message.reply(`${user.username},` + " " + BotReplyInsult);
-    console.log(fullhold.cyan + ` ${user.username} ` + ' said: ' + UserReplyColor);
-    console.log(fullhold.cyan+ " waznack replied : " + ReplybotInsultReply);
+		bot.send(botInsultReply, UserHold); //replies insult to Userhold
+
+    console.log(fullhold.cyan + ` ${user.username} ` + ' sent an insult message to : ' + UserHold.yellow); //console logs it
+    console.log(fullhold.cyan+ " waznack replied : " + botReplyColor.magenta); //console logs it
+        });
+  } else { 
+	// else insult username back
+var botsentdate = new Date();
+var bothour = botsentdate.getHours();
+var botminute = botsentdate.getMinutes();
+var botsecond = botsentdate.getSeconds();
+var fullhold = bothour + ":" + botminute + ":" + botsecond
+
+/*CREATES TIME STAMP 00:00:00 ^ | SENDS INSULT MESSAGE V*/
+
+UserReplyColor = message.body.green; //console.logs
+var BotReplyInsult = InsGen(); //holds Ins()
+
+bot.getUserProfile(message.from)
+	.then((user) => {
+			var ReplybotInsultReply = BotReplyInsult.magenta //console.log variable
+	message.reply(`${user.username},` + " " + BotReplyInsult); //!replies [username], InsGen();
+    console.log(fullhold.cyan + ` ${user.username} ` + ' said: ' + UserReplyColor); //console.log
+    console.log(fullhold.cyan+ " waznack replied : " + ReplybotInsultReply); //console.log
         });
   
 }
 });
 
 
- 
-console.log(botStart); //bot activate
-
-
-
+console.log(botStart); //parses through entire code. bot finish loading, then console.log it
 
 
 // Set up your server and start listening 
